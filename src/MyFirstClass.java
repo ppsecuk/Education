8e580557a7524a7eac676250e29276cfe81c8170
ppsecuk <pavel.psecuk@gmail.com>
2019-05-02 12:35:26 +0300

    upload 4

--- src/MyFirstClass.java
+++
@@ -1,9 +0,0 @@
-public class MyFirstClass {
-    public static void main(String[] args) {
-        for(int hour=0;hour<=12;hour++){
-            for (int minute=0;minute<60;minute++){
-                System.out.println(hour+":"+minute);
-            }
-        }
-    }
-}
