deefe042b69e9c857aab326b539b43de983dc523
ppsecuk <pavel.psecuk@gmail.com>
2019-05-02 14:27:04 +0300

    upload 6

---
+++ src/com/sda/tallinn6/fundamentals/practical/task8.java
@@ -0,0 +1,4 @@
+package com.sda.tallinn6.fundamentals.practical;
+
+public class task8 {
+}
---
+++ src/com/sda/tallinn6/fundamentals/practical/task9.java
@@ -0,0 +1,32 @@
+package com.sda.tallinn6.fundamentals.practical;
+
+import java.util.Scanner;
+
+public class task9 {
+    public static void main(String[] args) {
+            System.out.println("Please enter sentence:");
+            Scanner scanner = new Scanner(System.in);
+            String sentence = scanner.nextLine();
+
+            System.out.println("Please provide some letter");
+            //char x = scanner.next().charAt(0);
+
+        int count=0;
+        for(char x=scanner.next().charAt(0);x<='z';x++)
+        {
+            count=0;
+            //char x=a;
+            for(int i=0;i<sentence.length();i++)
+            {
+                if(x==sentence.charAt(i))
+                {
+                    count=count+1;
+                }
+            }
+            if(count>0)
+            {
+                System.out.println(x+":"+count);
+            }
+        }
+    }
+}
