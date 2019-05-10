f18a411b9ba3d0b5b363a8dcd3c8c0a5e79a4441
Pavel Psecuk <49373111+ppsecuk@users.noreply.github.com>
2019-05-10 13:45:03 +0300

    homework

---
+++ src/com/sda/tallinn6/fundamentals/practical/homework/extra/task1/Extra1.java
@@ -0,0 +1,20 @@
+package random_tasks.homework.extra;
+
+import java.util.Scanner;
+
+public class Extra1 {
+    public static void main(String[] args) {
+        System.out.println("Enter any sentence:");
+
+        while(true) {
+            Scanner scanner = new Scanner(System.in);
+            String sentence = scanner.nextLine();
+            String reverse = "";
+
+            for (int counter = sentence.length() - 1; counter >= 0; counter--) {
+                reverse = reverse + sentence.charAt(counter);
+            }
+            System.out.println("Reversed sentence is: \n" + reverse);
+        }
+    }
+}
