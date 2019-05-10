402e661ac82ccf547b2e43e2cbd9cc20b6a0fc04
Pavel Psecuk <49373111+ppsecuk@users.noreply.github.com>
2019-05-10 13:45:32 +0300

    homework

---
+++ src/com/sda/tallinn6/fundamentals/practical/homework/extra/task2/Extra2.java
@@ -0,0 +1,30 @@
+package random_tasks.homework.extra;
+
+import java.util.Scanner;
+
+public class Extra2 {
+
+    public static void main(String[] args) {
+        while(true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter a number:");
+            int numberToCheck = scanner.nextInt();
+            //reader.close();
+
+            int firstNumber = 0, secondNumber = 1, fibonacciNumber = 1;
+
+            while (fibonacciNumber < numberToCheck) {
+                fibonacciNumber = firstNumber + secondNumber;
+                firstNumber = secondNumber;
+                secondNumber = fibonacciNumber;
+            }
+            if (numberToCheck == fibonacciNumber) {
+                System.out.println("It is fib value");
+            } else if (numberToCheck <= 0) {
+                System.out.println("Negative values and 0 are not fib values.");
+            } else {
+                System.out.println("It is not fib value");
+            }
+        }
+    }
+}
