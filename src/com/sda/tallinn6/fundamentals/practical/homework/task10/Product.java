bcd1a2eb632788fd74267bd19f12405db372bb08
Pavel Psecuk <49373111+ppsecuk@users.noreply.github.com>
2019-05-10 13:46:34 +0300

    homework

---
+++ src/com/sda/tallinn6/fundamentals/practical/homework/task10/Product.java
@@ -0,0 +1,20 @@
+package random_tasks.homework.task10;
+
+public class Product {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    //public Product(){
+    //}
+}
---
+++ src/com/sda/tallinn6/fundamentals/practical/homework/task10/ProductList.java
@@ -0,0 +1,43 @@
+package random_tasks.homework.task10;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductList {
+
+    private List<Product> listOfProducts = new ArrayList<>();
+
+    public void printAllProducts() {
+        for (int i = 0; i < listOfProducts.size(); i++) {
+            Product existingProduct = listOfProducts.get(i);
+            System.out.println(listOfProducts.get(i).getName());
+        }
+    }
+
+    public void insertProduct(Product product) {
+        listOfProducts.add(product);
+    }
+
+    public void removeProduct(int index) {
+        listOfProducts.remove(0);
+    }
+
+    public int getIndexOfProduct(String name) {
+
+        for (int i = 0; i < listOfProducts.size(); i++) {
+            Product product = listOfProducts.get(i);
+            String nameOfThisProduct = product.getName();
+            if (nameOfThisProduct.equalsIgnoreCase(name)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean isNotFull() {
+        if (listOfProducts.size() < 5) {
+            return true;
+        }
+        return false;
+    }
+}
